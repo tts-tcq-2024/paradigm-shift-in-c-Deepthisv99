0619a767d47067bb006ae32e55f4a3b8354cf875
Deepthisv99 <deepthivenki1@gmail.com>
2024-06-25 17:28:05 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -30,56 +30,45 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
 
-#include <stdio.h>
+
+ #include <stdio.h>
 #include <stdbool.h>  // For boolean data type
- 
-bool isTemperatureOk(float temperature);
-bool isSocOk(float soc);
-bool isChargeRateOk(float chargeRate);
-bool batteryIsOk(float temperature, float soc, float chargeRate);
- 
-int main() {
-  // Test cases
-  if (batteryIsOk(25, 70, 0.7f)) {
-    printf("Test 1 passed: Battery OK\n");
-  } else {
-    printf("Test 1 failed: Battery not OK\n");
-  }
- 
-  if (!batteryIsOk(50, 85, 0.0f)) {
-    printf("Test 2 passed: Battery not OK (as expected)\n");
-  } else {
-    printf("Test 2 failed: Battery should not be OK\n");
-  }
- 
-  printf("All tests completed.\n");
-  return 0;
-}
- 
 bool isTemperatureOk(float temperature) {
-  if (temperature < 0 || temperature > 45) {
-    printf("Temperature is out of range!\n");
-    return false;
-  }
-  return true;
+    return !(temperature < 0 || temperature > 45);
 }
- 
 bool isSocOk(float soc) {
-  if (soc < 20 || soc > 80) {
-    printf("State of Charge is out of range!\n");
-    return false;
-  }
-  return true;
+    return !(soc < 20 || soc > 80);
 }
- 
 bool isChargeRateOk(float chargeRate) {
-  if (chargeRate > 0.8) {
-    printf("Charge Rate is out of range!\n");
-    return false;
-  }
-  return true;
+    return !(chargeRate < 0.0f || chargeRate > 0.8f);
 }
- 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  return (isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate));
+    if (!isTemperatureOk(temperature)) {
+        printf("Temperature is out of range!\n");
+        return false;
+    }
+    if (!isSocOk(soc)) {
+        printf("State of Charge (SOC) is out of range!\n");
+        return false;
+    }
+    if (!isChargeRateOk(chargeRate)) {
+        printf("Charge rate is out of range!\n");
+        return false;
+    }
+    return true;
+}
+int main() {
+    
+    if (batteryIsOk(25, 70, 0.7f)) {
+        printf("Test 1 passed: Battery OK\n");
+    } else {
+        printf("Test 1 failed: Battery not OK\n");
+    }
+    if (!batteryIsOk(50, 85, 0.0f)) {
+        printf("Test 2 passed: Battery not OK (as expected)\n");
+    } else {
+        printf("Test 2 failed: Battery should not be OK\n");
+    }
+    printf("All tests completed.\n");
+    return 0;
 }
